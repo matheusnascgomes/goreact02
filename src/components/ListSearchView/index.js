ed078ebc84387872e8d8c0159cbcf17f84edbf7c
Matheus Gomes <matheusnascgomes@gmail.com>
2018-06-13 21:52:18 -0300

    resolução de conflitos

--- src/components/ListSearchView/index.js
+++ src/components/ListSearchView/index.js
@@ -1,30 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Creators as repoActions } from '../../store/ducks/repos';
+
 import { ListSearch, ListItems } from './styles';
 
-const ListSearchView = ({ repo }) =>
-//   const repoItem = repo.map(item => (
-//     <ListItems
-//       key={item.id}
-//       onClick={() => console.log('Click')}
-//     >
-//       <img src={item.image} alt={item.organization} />
-//       <div className="block_info">
-//         <div className="text">
-//           <p>{item.name}</p>
-//           <small>{item.organization}</small>
-//         </div>
-//         <div className="icon_select"><i className="fa fa-angle-right" /></div>
-//       </div>
-//     </ListItems>
-//   ));
+const ListSearchView = ({ repo }) => {
+  const repoItem = repo.map(item => (
+    <ListItems
+      key={item.id}
+      onClick={() => console.log('Click')}
+    >
+      <img src={item.image} alt={item.organization} />
+      <div className="block_info">
+        <div className="text">
+          <p>{item.name}</p>
+          <small>{item.organization}</small>
+        </div>
+        <div className="icon_select"><i className="fa fa-angle-right" /></div>
+      </div>
+    </ListItems>
+  ));
 
-  (
-     <ListSearch>
-      {/* {repoItem} */}
+  return (
+    <ListSearch>
+      {{ repoItem } }
     </ListSearch>
   );
+};
+
 ListSearchView.propTypes = {
   repo: PropTypes.arrayOf(PropTypes.shape({
     id: PropTypes.number,
